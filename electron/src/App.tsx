cea6854655d763dfb7ea29d663d35227a6ffa249
ZNX <henrique.baronceli@gmail.com>
2025-03-30 10:34:49 +1300

    refactor and update

---
+++ electron/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>
+          Welcome to the R5 Desktop Wallet
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
