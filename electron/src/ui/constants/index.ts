77a4554960047c7b861f4a8002d7278c10fa22a2
ZNX <henrique.baronceli@gmail.com>
2025-04-18 12:47:37 +1200

    compile for testnet, fix typos

--- electron/src/ui/constants/index.ts
+++ electron/src/ui/constants/index.ts
@@ -4,5 +4,5 @@ export const AppVersion = "v1.0.1-beta" // Don't change this!
 export const VersionApiUrl = "https://api.github.com/repos/r5-labs/r5-wallet/releases/latest" // Don't change this!
 export const UpdateDownloadUrl = "https://github.com/r5-labs/r5-wallet/releases/latest" // Don't change this!
 export const HelpUrl = "https://docs.r5.network/tutorials-and-guides/r5-desktop-wallet" // Don't change this!
-export const RpcUrl = "https://rpc.r5.network" // Use your own RPC server or a public one
-export const ExplorerUrl = "https://explorer.r5.network" // You can change the preferred explorer URL
+export const RpcUrl = "https://rpc-testnet.r5.network" // Use your own RPC server or a public one
+export const ExplorerUrl = "https://explorer-testnet.r5.network" // You can change the preferred explorer URL
