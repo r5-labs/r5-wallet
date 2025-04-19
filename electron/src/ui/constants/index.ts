c8fd757eb909b01ddbf16705e92c7ef792ee3090
ZNX <henrique.baronceli@gmail.com>
2025-04-20 08:30:25 +1200

    set 1.0.2

--- electron/src/ui/constants/index.ts
+++ electron/src/ui/constants/index.ts
@@ -1,6 +1,6 @@
 export const AppName = "R5 Desktop Wallet" // Don't change this!
 export const AppDescription = "An open source and community-developed desktop wallet for the R5 Network." // Don't change this!
-export const AppVersion = "1.0.1-beta" // Don't change this!
+export const AppVersion = "1.0.2-beta" // Don't change this!
 export const VersionApiUrl = "https://api.github.com/repos/r5-labs/r5-wallet/releases/latest" // Don't change this!
 export const UpdateDownloadUrl = "https://github.com/r5-labs/r5-wallet/releases/latest" // Don't change this!
 export const HelpUrl = "https://docs.r5.network/tutorials-and-guides/r5-desktop-wallet" // Don't change this!
